24952ce957f374af2fd0f079c899d9a57fb2d517
Cancain <eriksson.tomas@gmail.com>
2019-05-02 12:23:22 +0200

    makde connection with wp-backend

--- src/Containers/Axios/Axios.js
+++ src/Containers/Axios/Axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const wpInstance = axios.create({
-  baseURL:
-    "http://localhost/skaggbyran-gavle-react/WordPress/wp-admin/wp-json/v2"
+  baseURL: "http://localhost/skaggbyran-gavle-react/WordPress/wp-json/wp/v2"
 });
--- src/Containers/Pages/Home/Home.js
+++ src/Containers/Pages/Home/Home.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { wpInstance } from "../../Axios/Axios";
 
 import style from "./Home.module.css";
 
 const Home = props => {
+  useState(() => {
+    wpInstance
+      .get()
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  });
+
   return (
     <div className={style.Home}>
       <h1>Home</h1>
